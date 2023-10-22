c2e1cadaf6e6e2e37435a7695d6486a55f26def7
kelvin handoko <kelvinhandoko03@gmail.com>
2023-10-22 21:01:45 +0700

    feat: register functionality done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,6 +4,7 @@ import {
     Divider,
     Fade,
     FormControl,
+    FormHelperText,
     IconButton,
     InputAdornment,
     InputLabel,
@@ -11,21 +12,66 @@ import {
     OutlinedInput,
     Stack,
     Typography,
-} from "@mui/material";
-import registerAsset from "../assets/register.gif";
-import { useState } from "react";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
-import GoogleIcon from "../component/icon/GoogleIcon";
-import { Helmet } from "react-helmet";
+} from "@mui/material"
+import registerAsset from "../assets/register.gif"
+import { useState } from "react"
+import { Visibility, VisibilityOff } from "@mui/icons-material"
+import GoogleIcon from "../component/icon/GoogleIcon"
+import { Helmet } from "react-helmet"
+import { useForm } from "react-hook-form"
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth"
+import { auth, googleProvider } from "../config/firebase"
+import { enqueueSnackbar } from "notistack"
+import { useNavigate } from "react-router-dom"
 
 const RegisterPage = () => {
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false)
 
-    const handleClickShowPassword = () => setShowPassword(show => !show);
+    const handleClickShowPassword = () => setShowPassword(show => !show)
 
     const handleMouseDownPassword = event => {
         event.preventDefault()
-    };
+    }
+
+    const navigate = useNavigate()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = async ({ email, password }) => {
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+            enqueueSnackbar("success register", {
+                variant: "success",
+                anchorOrigin: { horizontal: "right", vertical: "top" },
+                autoHideDuration: 2000,
+            })
+            setTimeout(() => {
+                navigate("/login")
+            }, 500)
+        } catch (error) {
+            enqueueSnackbar("failed to register", {
+                variant: "error",
+                anchorOrigin: { horizontal: "right", vertical: "top" },
+                autoHideDuration: 2000,
+            })
+        }
+    }
+
+    const handleGoogleLogin = async () => {
+        try {
+            const res = await signInWithPopup(auth, googleProvider)
+            const credential = GoogleAuthProvider.credentialFromResult(res)
+            if (credential) {
+                navigate("/")
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <Stack spacing={2} direction="row">
@@ -41,43 +87,49 @@ const RegisterPage = () => {
                 }}
             >
                 <Fade in timeout={800}>
-                    <Stack sx={{ width: "min(80%,480px)", mx: "auto" }} spacing={4}>
+                    <Stack
+                        component="form"
+                        onSubmit={handleSubmit(onSubmit)}
+                        sx={{ width: "min(80%,480px)", mx: "auto" }}
+                        spacing={4}
+                    >
                         <Stack spacing={1}>
                             <Typography variant="h3" fontWeight="800">
                                 Register
                             </Typography>
-                            <Typography
-                                variant="h7"
-                                color="GrayText"
-                                fontWeight="bold"
-                            >
+                            <Typography variant="h7" color="GrayText" fontWeight="bold">
                                 welcome to edutrack! create account and start learning
                             </Typography>
                         </Stack>
+
                         <FormControl variant="outlined">
-                            <InputLabel htmlFor="standard-adornment-name">Name</InputLabel>
+                            <InputLabel htmlFor="email">Email</InputLabel>
                             <OutlinedInput
-                                id="standard-adornment-name"
-                                type="text"
-                                label="Name"
-                            />
-                        </FormControl>
-                        <FormControl variant="outlined">
-                            <InputLabel htmlFor="standard-adornment-email">Email</InputLabel>
-                            <OutlinedInput
-                                id="standard-adornment-email"
+                                {...register("email", {
+                                    required: { message: "password is required" },
+                                })}
                                 type="email"
-                                label="Email"
+                                label="email"
                             />
+                            {errors.email ? (
+                                <FormHelperText>{errors.email.message}</FormHelperText>
+                            ) : (
+                                ""
+                            )}
                         </FormControl>
                         <Stack spacing={1}>
                             <FormControl variant="outlined">
-                                <InputLabel htmlFor="standard-adornment-password">
-                                    Password
-                                </InputLabel>
+                                <InputLabel htmlFor="password">Password</InputLabel>
                                 <OutlinedInput
-                                    id="standard-adornment-password"
                                     type={showPassword ? "text" : "password"}
+                                    required
+                                    {...register("password", {
+                                        pattern: {
+                                            value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/,
+                                            message: "Password must meet the required pattern",
+                                        },
+                                        required: { message: "password is required" },
+                                    })}
                                     label="Password"
                                     endAdornment={
                                         <InputAdornment position="end">
@@ -91,9 +143,20 @@ const RegisterPage = () => {
                                         </InputAdornment>
                                     }
                                 />
+                                {errors.password ? (
+                                    <FormHelperText>{errors.password.message}</FormHelperText>
+                                ) : (
+                                    ""
+                                )}
                             </FormControl>
                         </Stack>
-                        <Button color="secondary" variant="contained" size="large" fullWidth>
+                        <Button
+                            color="secondary"
+                            type="submit"
+                            variant="contained"
+                            size="large"
+                            fullWidth
+                        >
                             Register
                         </Button>
                         <Divider>OR</Divider>
@@ -103,6 +166,7 @@ const RegisterPage = () => {
                             color="primary"
                             size="large"
                             fullWidth
+                            onClick={handleGoogleLogin}
                         >
                             Continue with Google
                         </Button>
@@ -128,7 +192,7 @@ const RegisterPage = () => {
                 <img src={registerAsset} width={450} />
             </Box>
         </Stack>
-    );
-};
+    )
+}
 
 export default RegisterPage
