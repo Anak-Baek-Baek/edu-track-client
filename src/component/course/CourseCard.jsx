1560dc553f8d0da5325bd1dbbb622b112f46c496
kelvin handoko <kelvinhandoko03@gmail.com>
2023-10-23 00:10:35 +0700

    feat: course card component ui done

--- src/component/course/CourseCard.jsx
+++ src/component/course/CourseCard.jsx
@@ -1,7 +1,90 @@
-import { Box } from "@mui/material"
+/* eslint-disable react/prop-types */
+import {
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    IconButton,
+    LinearProgress,
+    Typography,
+} from "@mui/material"
+import exampleImage from "../../assets/example.jpg"
+import { Book, BookOnline, MenuBook } from "@mui/icons-material"
+import { useEffect, useState } from "react"
+const CourseCard = props => {
+    const { imageUrl, title, lecturer, totalSection, progressPercent } = props
+    const [progress, setProgress] = useState(0)
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress(progressPercent)
+        }, 500)
 
-const CourseCard = () => {
-    return <Box border="1px solid">CourseCard</Box>
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+    return (
+        <Card
+            variant="outlined"
+            sx={{
+                padding: "0.625rem",
+                width: "100%",
+                borderRadius: "1rem",
+                border: "1px solid #CFCFCF",
+                bgcolor: "transparent",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+            }}
+        >
+            <CardMedia
+                sx={{ height: 160, borderRadius: ".5rem" }}
+                image={imageUrl ?? exampleImage}
+                title="green iguana"
+            />
+            <CardContent sx={{ padding: 0, display: "flex", flexDirection: "column", gap: 2 }}>
+                <Box>
+                    <Typography variant="h5" noWrap textTransform="capitalize" fontWeight="700">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" fontSize="small">
+                        {lecturer}
+                    </Typography>
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "center", gap: ".3rem" }}>
+                    <IconButton
+                        sx={{ borderRadius: 100, padding: 0.6, border: "1.2px solid #6A00FF" }}
+                    >
+                        <MenuBook color="secondary" fontSize="small" />
+                    </IconButton>
+                    <Typography variant="p" fontSize="larger">
+                        {totalSection > 1 ? `${totalSection} sections` : `${totalSection} section`}
+                    </Typography>
+                </Box>
+                <Box display="flex" flexDirection="column" gap={1}>
+                    <LinearProgress
+                        variant="determinate"
+                        value={progress}
+                        sx={{ borderRadius: 100, height: 8 }}
+                        color={
+                            progress < 30
+                                ? "info"
+                                : progress < 70
+                                ? "primary"
+                                : progress < 100
+                                ? "success"
+                                : "inherit"
+                        }
+                    />
+                    <Typography variant="p" fontSize="larger">
+                        {progress}% completed
+                    </Typography>
+                </Box>
+            </CardContent>
+        </Card>
+    )
 }
 
 export default CourseCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import CourseCard from "../component/course/CourseCard"
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <Box
             sx={{
-                width: "min(95%,1080px)",
+                width: "min(95%,1440px)",
                 mx: "auto",
                 p: 4,
                 display: "flex",
@@ -17,10 +17,15 @@ const Home = () => {
             <Typography variant="h4" fontWeight="700">
                 learn new thing
             </Typography>
-            <Grid2 container spacing={4}>
+            <Grid2 container columns={12} spacing={4}>
                 {Array.from(Array(8)).map((_, index) => (
-                    <Grid2 xs={12} sm={4} lg={3} key={index}>
-                        <CourseCard />
+                    <Grid2 xs={12} sm={6} md={4} lg={3} key={index}>
+                        <CourseCard
+                            title="computer science asdoasdoijosda doasijdoasjodjasoi doasjodasjoij"
+                            totalSection={5}
+                            progressPercent={80}
+                            lecturer="john"
+                        />
                     </Grid2>
                 ))}
             </Grid2>
