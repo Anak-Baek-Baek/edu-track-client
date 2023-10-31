823868d92038d8aff4e6219544d549b2474064e4
kelvin handoko <89412241+kelvinhandoko@users.noreply.github.com>
2023-10-31 23:50:37 +0700

    Menambahkan data (#9)
    Co-authored-by: LImzen <LImzen>

---
+++ src/data/data.js
@@ -0,0 +1,458 @@
+const Course = [
+  {
+    name: "Belajar React",
+    category: "react",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1633356122544-f134324a6cee?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 50000,
+    section: [
+      {
+        title: "Pengenalan React",
+        description: "Mengenalkan apa itu React JS",
+        videoUrl: "https://youtu.be/kcnwI_5nKyA?si=4cy__3_VzgLxiGBw",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Instalasi React",
+        description: "Cara instalasi React yang benar",
+        videoUrl: "https://youtu.be/RVH_5L5Lsp0?si=3nW5JMtPYesChYya",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar HTML",
+    category: "html",
+    lecturer: "Sandhi",
+    totalSections: 3,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1632882765546-1ee75f53becb?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 45000,
+    section: [
+      {
+        title: "Pendahuluan",
+        description: "Mengenalkan apa itu HTML",
+        videoUrl: "https://youtu.be/NBZ9Ro6UKV8?si=pUE1YhKQj6iFf48D",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Dasar-dasar HTML",
+        description: "Membuat website sederhana",
+        videoUrl: "https://youtu.be/1NicaVOCXHA?si=c16ahZ2akKPJ-ErG",
+        position: 2,
+        isPublished: true,
+        isFree: false,
+      },
+      {
+        title: "Code Editor",
+        description: "Membuat website sederhana",
+        videoUrl: "https://youtu.be/3sLSi9L5nWE?si=uKDbLN7Bb-MMTCeF",
+        position: 3,
+        isPublished: false,
+        isFree: false,
+      },
+    ],
+  },
+  {
+    name: "Belajar CSS3",
+    category: "css",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1610986603166-f78428624e76?auto=format&fit=crop&q=80&w=2058&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 40000,
+    section: [
+      {
+        title: "Pengenalan CSS3",
+        description: "Mengenalkan apa itu CSS3",
+        videoUrl: "https://youtu.be/J0a6YUUAsd4?si=Ct0xWeovOC3lTQoy",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Border Radius",
+        description: "Penjelasan Properti CSS3 Border Radius ",
+        videoUrl: "https://youtu.be/3xbW5YHln78?si=-YSMyMt6pn12M5C2",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar Webpack",
+    category: "webpack",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl: "",
+    price: 60000,
+    section: [
+      {
+        title: "Pengenalan Webpack",
+        description: "Memahami apa itu Module System pada Javascript.",
+        videoUrl: "https://youtu.be/YwClq14cvX0?si=KPD9wfW8ZZZTMHA4",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Belajar Webpack",
+        description: "Membahas mengenai Webpack dan juga Module Bundlers.",
+        videoUrl: "https://youtu.be/zwzzn8-fCJ8?si=QiS0nBTw-Dlshiwv",
+        position: 2,
+        isPublished: false,
+        isFree: false,
+      },
+    ],
+  },
+  {
+    name: "Belajar HTML5",
+    category: "html",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1621839673705-6617adf9e890?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 50000,
+    section: [
+      {
+        title: "Apa itu HTML5",
+        description: "Mari kita mulai belajar mengenai HTML5",
+        videoUrl: "https://youtu.be/Q2VqCG13ejA?si=nFwCjnkYa88hGk4i",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Semantic HTML5?",
+        description: "Penjelasan mengenai Semantic HTML5",
+        videoUrl: "https://youtu.be/o3m15BWi2HM?si=UW9vNEXh-KREMDSe",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar BOOTSTRAP 5",
+    category: "bootstrap",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1606166325695-ce4d64e3195f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 70000,
+    section: [
+      {
+        title: "Membuat Website Portfolio dengan BOOTSTRAP 5",
+        description: "Tutorial membuat website dengan BOOTSTRAP 5",
+        videoUrl: "https://youtu.be/LkR-9Z1sle8?si=klpBEdphvNrK2Na3",
+        position: 1,
+        isPublished: true,
+        isFree: false,
+      },
+      {
+        title: "Cara Mengupload Website & Hosting",
+        description:
+          "Mengupload Website Ke Internet Menggunakan GitHub Pages & Web Hosting",
+        videoUrl: "https://youtu.be/65Jv9Y13eVo?si=50yu5CwxTlInhN5E",
+        position: 2,
+        isPublished: true,
+        isFree: false,
+      },
+    ],
+  },
+  {
+    name: "Belajar Tailwind CSS",
+    category: "css",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1505685296765-3a2736de412f?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 80000,
+    section: [
+      {
+        title: "Apa itu Tailwind CSS?",
+        description: "Mengenalkan apa itu React JS",
+        videoUrl: "https://youtu.be/z3slaXqmkT0?si=3oT3oa2sYd0lUu0h",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Kenapa Tailwind CSS?",
+        description: "Cari tau alasan kenapa dibuatnya framework TailwindCSS",
+        videoUrl: "https://youtu.be/ELCr9MyRTH8?si=pf01zpWdvUagmgvn",
+        position: 2,
+        isPublished: false,
+        isFree: false,
+      },
+    ],
+  },
+  {
+    name: "Belajar Laravel 8",
+    category: "php",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1607970669494-309137683be5?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 65000,
+    section: [
+      {
+        title: "Pengenalan Laravel 8",
+        description:
+          "Kali ini kita akan memulai seri baru untuk belajar Laravel 8.",
+        videoUrl: "https://youtu.be/HqAMb6kqlLs?si=iyTxWpM2qRJfCECa",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Instalasi Laravel 8",
+        description:
+          "Menginstall laravel 8 pada sistem operasi MacOS dan Windows",
+        videoUrl: "https://youtu.be/pZqk57Xvujssi=CLkLS1iQumDCm8XI",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar JavaScript & DOM",
+    category: "javascript",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1592609931095-54a2168ae893?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 50000,
+    section: [
+      {
+        title: "Pendahuluan",
+        description:
+          "Di seri ini kita akan mempelajari mengenai DOM atau Document Object Model pada HTML dan bagaiman cara bekerja dengan DOM menggunakan Javascript",
+        videoUrl: "https://youtu.be/aT60R1cySLMsi=oFj3tAAQX0a2Du-x",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "DOM Tree",
+        description:
+          "Di video kali ini kita akan membahas lebih detil mengenai konsep DOM Tree.. tipe2 apa saja yang ada pada sebuah node, perbedaan antara nodeList dan HTMLCollection serta konsep Hirarki pada DOM. ",
+        videoUrl: "https://youtu.be/zY5RlT5q5EMsi=qmVGVxRc1GddryeF",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar Javascript",
+    category: "javascript",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1627398242454-45a1465c2479?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 55000,
+    section: [
+      {
+        title: "Dasar Pemrograman dengan Javascript",
+        description:
+          "Di seri ini kita akan mempelajari mengenai konsep dasar pemrograman, istilah2 yang ada dalam dunia pemrograman, cara menulis dan mengeksekusi program, dll.",
+        videoUrl: "https://youtu.be/RUTV_5m4VeIsi=n1Lp28eC8aoQwhdQ",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Apa itu Pemrograman?",
+        description:
+          "Sebelum mulai ngoding, ada baiknya kita tau dulu apa itu pemrograman dan program itu sendiri.",
+        videoUrl: "https://youtu.be/Ncrlg9kTC6Usi=8dgLZKC6riLawpjr",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar membuat aplikasi menggunakan PHP",
+    category: "php",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.unsplash.com/photo-1599507593362-50fa53ed1b40?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    price: 47000,
+    section: [
+      {
+        title: "Membuat Aplikasi MVC dengan PHP | Pendahuluan",
+        description:
+          "MVC adalah sebuah konsep dalam pembuatan perangkat lunak berorientasi object. Konsep MVC ini sangat penting untuk kita pelajari agar kita dapat membuat aplikasi yang baik, terutama ketika nantinya kita akan membuat aplikasi menggunakan Web Application Framework seperti CodeIgniter, Laravel atau yang lainnya",
+        videoUrl: "https://youtu.be/tBKOb8Ib5nI?si=RsKTNNV8jw5qGJlP",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Membuat Aplikasi MVC dengan PHP | Persiapan",
+        description:
+          "Di video kali ini kita akan melakukan persiapan pembuatan folder dan file2 yang dibutuhkan untuk membuat aplikasi MVC kita",
+        videoUrl: "https://youtu.be/t3A4ZtOONRs?si=2zXpBRu1as-3n3IO",
+        position: 2,
+        isPublished: false,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar HTML Dari Nol Sampai Mahir",
+    category: "html",
+    lecturer: "Elon",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.pexels.com/photos/270404/pexels-photo-270404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 26000,
+    section: [
+      {
+        title: "Pendahuluan HTML",
+        description:
+          "Hypertext Markup Language atau HTML adalah bahasa markup standar yang digunakan untuk membuat halaman website. Jadi, HTML dapat dianalogikan sebagai pondasi awal dalam menyusun kerangka halaman web secara terstruktur dan langkah awal sebelum membuat website adalah belajar HTML. di Playlist Belajar HTML ini kita akan bahas tentang Tag yang ada pada HTML beserta fungsinya.",
+        videoUrl: "https://youtu.be/0oA1Z6UKM5M?si=o37Y_52OLpI2LfJ7",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Membuat Paragraf menggunakan HTML",
+        description:
+          "Tag P pada HTML digunakan untuk menandai  elemen sebagai Paragraf",
+        videoUrl: "https://youtu.be/qwKm_7GmgBU?si=sBBdyChEuNZ2pAWq",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "CSS Dasar",
+    category: "css",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 30000,
+    section: [
+      {
+        title: "Pendahuluan CSS",
+        description:
+          "di video pertama ini kita akan belajar mengenai pendahuluan CSS, apa itu CSS, definisinya dan manfaatnya apa.",
+        videoUrl: "https://youtu.be/CleFk3BZB3g?si=-vpAGloORac_Lr-k",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Anatomi CSS",
+        description:
+          "Sama seperti HTML, CSS juga memiliki anatomi / struktur dari tiap-tiap deklarasinya. Anatomi tersebut terdiri dari Selector, Property, Value",
+        videoUrl: "https://youtu.be/8lXDi2Mxp9c?si=nePgZAJdPD5NXoB5",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar PHP untuk PEMULA",
+    category: "php",
+    lecturer: "Sandhi",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.pexels.com/photos/270557/pexels-photo-270557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 43000,
+    section: [
+      {
+        title: "Intro belejar PHP untuk Pemula",
+        description:
+          "Seri baru di Channel WebProgrammingUNPAS!! setelah banyak banget temen2 yang minta dibuatkan seri ini, maka kali ini kita mulai pembelajaran untuk PHP dasar khusus untuk pemula.",
+        videoUrl: "https://youtu.be/l1W2OwV5rgY?si=-WbKVGX2exCjs8j-",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Sejarah dan Karakteristik PHP",
+        description:
+          "Sebelum masuk belajar mengenai sintaks PHP, yuk kita ketahui dulu sejarahnya kenapa sampai ada bahasa PHP dan perkembangannya sampai saat ini.",
+        videoUrl: "https://youtu.be/q3NVC5JxgVI?si=dTLuTujfs7w6ZZpo",
+        position: 2,
+        isPublished: true,
+        isFree: false,
+      },
+    ],
+  },
+  {
+    name: "Belajar Python",
+    category: "python",
+    lecturer: "Poo",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 52000,
+    section: [
+      {
+        title: "Apa itu Python",
+        description: "Mengenalkan apa itu Python",
+        videoUrl: "https://youtu.be/iA8lLwmtKQM?si=4v65gk4XddSs3oB7",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Instalasi Python",
+        description: "Cara instalasi Python",
+        videoUrl: "https://youtu.be/xETkm9H6aaY?si=oPPQBJsHAXY4h9fx",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+  {
+    name: "Belajar Javascript untuk Pemula",
+    category: "javascript",
+    lecturer: "Raymond",
+    totalSections: 2,
+    bacgkroundUrl:
+      "https://images.pexels.com/photos/1089440/pexels-photo-1089440.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: 67000,
+    section: [
+      {
+        title: "Perkenalan Dan Persiapan",
+        description:
+          "Belajar javascript pemula dimana pembahasannya dimulai dari belajar javascript dari nol sampai belajar javascript lanjutan atau biasa kita kenal dengan belajar javascript dom.",
+        videoUrl: "https://youtu.be/bi0pkZmB5J4?si=-B8-zFYCd8mPHC9c",
+        position: 1,
+        isPublished: true,
+        isFree: true,
+      },
+      {
+        title: "Debugging Tools Menggunakan Console",
+        description:
+          "Belajar javascript pemula dimana pembahasannya dimulai dari belajar javascript dari nol sampai belajar javascript lanjutan atau biasa kita kenal dengan belajar javascript dom.",
+        videoUrl: "https://youtu.be/BheHgHeU1U0?si=KUn8CBB7w77SVfyj",
+        position: 2,
+        isPublished: true,
+        isFree: true,
+      },
+    ],
+  },
+];
