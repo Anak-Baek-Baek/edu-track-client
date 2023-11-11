c5bead93bb1f7bcd190f7ed3f353f860dd304ace
kelvin handoko <kelvinhandoko03@gmail.com>
2023-11-11 20:18:17 +0700

    feat: update shuffled algo

--- src/utils/getRandomCourse.js
+++ src/utils/getRandomCourse.js
@@ -1,12 +1,8 @@
 import courses from "../data/data"
 
 const getRandomCourse = take => {
-    let shuffled = courses.slice(0)
-    for (let i = courses.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
-        ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
-    }
-    return shuffled.slice(0, take)
+    const shuffleArray = arr => arr.sort(() => Math.random() - 0.5)
+    return shuffleArray(courses).slice(0, take)
 }
 
 export default getRandomCourse
